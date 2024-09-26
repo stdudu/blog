ad697da62c3e8ef4c5cffe629566e876c52f79ae
dudu <nicolalmeida_se@outlook.com>
2024-09-26 12:22:20 -0300

    #

--- index.php
+++ index.php
@@ -4,9 +4,96 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pagina Inicial | Projeto para Web com PHP</title>
+    <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css">
 </head>
 <body>
-    <a href="/tests/sql_test.php">sql_test.php</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Topo -->
+                <?php
+                    include 'includes/topo.php';
+                ?>
+            </div>
+        </div>
+        <div class="row" style="min-height: 500px;">
+            <div class="col-md-12">
+                <!-- Menu -->
+                <?php
+                    include 'includes/menu.php'
+                ?>
+            </div>
+            <div class="col-md-10">
+                <h2>Pagina Inicial</h2>
+                <?php
+                    include 'includes/busca.php';
+                ?>
+                <?php
+                    require_once 'includes/funcoes.php';
+                    require_once 'core/conexao_mysql.php';
+                    require_once 'core/sql.php';
+                    require_once 'core/mysql.php';
+
+                    foreach($_GET as $indice => $dado){
+                        $$indice = limparDados($dado);
+                    }
+                    $data_atual = date('Y-m-d H:i:s');
+
+                    $criterio = [
+                        ['data_postagem', '<=', $data_atual]
+                    ];
+
+                    if(!empty($busca)){
+                        $criterio[] = [
+                            'AND',
+                            'texto',
+                            'like',
+                            "%{$busca}%"
+                        ];
+                    }
+
+                    $post = buscar(
+                        'post',
+                        [
+                              'titulo',
+                              'data_postagem',
+                              'id',
+                              '(select nome
+                                    from usuario
+                                    where usuario.id = post.usuario_id) as nome'  
+                        ],
+                        $criterio,
+                        'data_postagem DESC'
+                    );
+                ?>
+            </div>
+            <div>
+                <div class="list-group">
+                    <?php
+                    foreach($posts as $post) :
+                        $data = date_create($post['data_postagem']);
+                        $data = date_format($data, 'd/m/y H:i:s');
+                    ?>
+                    <a class="list-group-item list-group-item-action"
+                        href="post_detalhe.php?post=<?php echo $post['id']?>">
+                        <strong><?php echo $post['titulo']?></strong>
+                        [<?php echo $post['nome']?>]
+                        <span class="badge badge-dark"><?php echo $data?></span>
+                    </a>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Rodapé -->
+            <?php
+                include 'includes/rodape.php';
+            ?>
+        </div>
+    </div>
+    <script src="lib/bootstrap-4.2.1-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
